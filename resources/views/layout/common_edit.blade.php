e6a843f9d2549a76735dfd3c5c8db3577179dba5
tuwenjun <tuwenjun@wudi360.com>
2019-11-14 10:28:37 +0800

    no message

--- resources/views/layout/common_edit.blade.php
+++ resources/views/layout/common_edit.blade.php
@@ -6,9 +6,7 @@
     });
 
     @if(session('status'))
-        console.log(window.name);
         var index = parent.layer.getFrameIndex(window.name); //先得到当前iframe层的索引
-    console.log(index);
         parent.layer.close(index); //再执行关闭
         parent.layer.msg("{{session('status')}}",{icon:6});
     @endif
