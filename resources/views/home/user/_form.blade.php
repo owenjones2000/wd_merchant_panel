79d56be01475a7f217855e82e143f066f506401b
tuwenjun <tuwenjun@wudi360.com>
2019-11-20 15:19:58 +0800

    no message

--- resources/views/home/user/_form.blade.php
+++ resources/views/home/user/_form.blade.php
@@ -1,49 +1,49 @@
 {{csrf_field()}}
 <div class="layui-form-item">
-    <label for="" class="layui-form-label">登录账号:</label>
+    <label for="" class="layui-form-label">User Name:</label>
     <div class="layui-input-inline">
-        <input type="text" name="username" value="{{ $user->username ?? old('username') }}" lay-verify="required" placeholder="请输入登录账号" class="layui-input" >
+        <input type="text" name="username" value="{{ $user->username ?? old('username') }}" lay-verify="required" placeholder="please input login account" autocomplete="username" class="layui-input" >
     </div>
 </div>
 
 <div class="layui-form-item">
-    <label for="" class="layui-form-label">真实姓名:</label>
+    <label for="" class="layui-form-label">Real Name:</label>
     <div class="layui-input-inline">
-        <input type="text" name="realname" value="{{ $user->realname ?? old('realname') }}" lay-verify="required" placeholder="请输入真实姓名" class="layui-input" >
+        <input type="text" name="realname" value="{{ $user->realname ?? old('realname') }}" lay-verify="required" placeholder="please input real name" autocomplete="off" class="layui-input" >
     </div>
 </div>
 
 <div class="layui-form-item">
-    <label for="" class="layui-form-label">电子邮箱:</label>
+    <label for="" class="layui-form-label">Email:</label>
     <div class="layui-input-inline">
-        <input type="email" name="email" value="{{$user->email??old('email')}}" lay-verify="email" placeholder="请输入电子邮箱" class="layui-input" >
+        <input type="email" name="email" value="{{$user->email??old('email')}}" lay-verify="email" placeholder="please input email" autocomplete="off" class="layui-input" >
     </div>
 </div>
 
 <div class="layui-form-item">
-    <label for="" class="layui-form-label">联系电话:</label>
+    <label for="" class="layui-form-label">Phone:</label>
     <div class="layui-input-inline">
-        <input type="text" name="phone" value="{{$user->phone??old('phone')}}" {{-- required="phone" lay-verify="phone" --}} placeholder="请输入手机号" class="layui-input">
+        <input type="text" name="phone" value="{{$user->phone??old('phone')}}" {{-- required="phone" lay-verify="phone" --}} placeholder="please input phone number" autocomplete="username" class="layui-input">
     </div>
 </div>
 
 <div class="layui-form-item">
-    <label for="" class="layui-form-label">登录密码</label>
+    <label for="" class="layui-form-label">Password</label>
     <div class="layui-input-inline">
-        <input type="password" name="password" placeholder="请输入登录密码" class="layui-input">
+        <input type="password" name="password" placeholder="please input password" autocomplete="current-password" class="layui-input">
     </div>
 </div>
 
 <div class="layui-form-item">
-    <label for="" class="layui-form-label">确认密码</label>
+    <label for="" class="layui-form-label">Confirm Password</label>
     <div class="layui-input-inline">
-        <input type="password" name="password_confirmation" placeholder="请再次输入登录密码" class="layui-input">
+        <input type="password" name="password_confirmation" placeholder="please confirm password" autocomplete="new-password" class="layui-input">
     </div>
 </div>
 <div class="layui-form-item">
     <div class="layui-input-block">
-        <button type="submit" class="layui-btn" lay-submit="" lay-filter="formDemo">确 认</button>
-        <div  class="layui-btn close-iframe" onclick="close_parent('{{$user->username ??''}}','/home/user')">关闭</div>
+        <button type="submit" class="layui-btn" lay-submit="" lay-filter="formDemo">Submit</button>
+        <div  class="layui-btn close-iframe" onclick="close_parent('{{$user->username ??''}}','/home/user')">Close</div>
     </div>
 </div>
 @section('script')
--- resources/views/home/user/create.blade.php
+++ resources/views/home/user/create.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="layui-card">
         <div class="layui-card-header  layuiadmin-card-header-auto">
-            <h2>添加用户</h2>
+            <h2>Add sub user</h2>
         </div>
         <div class="layui-card-body">
             <form class="layui-form" action="{{route('home.user.store')}}" method="post" onsubmit="return dosubmit()">
