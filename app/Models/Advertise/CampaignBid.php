8c6def23f520ebb21dde948f8f9d9b7e81134ad7
tuwenjun <tuwenjun@wudi360.com>
2019-11-27 10:57:26 +0800

    no message

--- app/Models/Advertise/CampaignBid.php
+++ app/Models/Advertise/CampaignBid.php
@@ -3,12 +3,9 @@
 namespace App\Models\Advertise;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CampaignBid extends Model
 {
-    // use SoftDeletes;
-
     protected $table = 'a_campaign_bid';
 
     protected $fillable = ['type', 'amount', 'country', 'deleted_at'];
--- app/Models/Advertise/CampaignBudget.php
+++ app/Models/Advertise/CampaignBudget.php
@@ -3,12 +3,9 @@
 namespace App\Models\Advertise;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CampaignBudget extends Model
 {
-    use SoftDeletes;
-
     protected $table = 'a_campaign_daily_budget';
 
     protected $fillable = ['amount', 'country'];
