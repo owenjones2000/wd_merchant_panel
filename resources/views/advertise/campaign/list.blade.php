18ec8e155519324dabb08bb47ebccdeb535f3c0e
Tim <twj101@163.com>
2020-02-18 15:49:38 +0800

    no message

--- resources/views/advertise/campaign/list.blade.php
+++ resources/views/advertise/campaign/list.blade.php
@@ -116,15 +116,15 @@
                         // ,{field: 'app.name', title: 'App', templet: '#appTpl'}
                         ,{field: 'budget', title: 'Budget', templet: function(d){return '$' + (d.default_budget || '0.00');}}
                         ,{field: 'created', title: 'Created', width:110, templet: function(d){return util.toDateString(d.created_at, "yyyy-MM-dd");}}
-                        ,{field: 'impressions', title: 'Impressions', templet: function(d){return d.impressions || 0;}, width:90}
-                        ,{field: 'clicks', title: 'Clicks', templet: function(d){return d.clicks || 0;}}
-                        ,{field: 'installs', title: 'Installs', templet: function(d){return d.installs || 0;}}
-                        ,{field: 'ctr', title: 'CTR', templet: function(d){return (d.ctr || '0.00') + '%';}}
-                        ,{field: 'cvr', title: 'CVR', templet: function(d){return (d.cvr || '0.00') + '%';}}
-                        ,{field: 'ir', title: 'IR', templet: function(d){return (d.ir || '0.00') + '%';}}
+                        ,{field: 'impressions', title: 'Impressions', templet: function(d){return d.impressions || 0;}, width:80}
+                        ,{field: 'clicks', title: 'Clicks', templet: function(d){return d.clicks || 0;}, width:80}
+                        ,{field: 'installs', title: 'Installs', templet: function(d){return d.installs || 0;}, width:80}
+                        ,{field: 'ctr', title: 'CTR', templet: function(d){return (d.ctr || '0.00') + '%';}, width:80}
+                        ,{field: 'cvr', title: 'CVR', templet: function(d){return (d.cvr || '0.00') + '%';}, width:80}
+                        ,{field: 'ir', title: 'IR', templet: function(d){return (d.ir || '0.00') + '%';}, width:80}
                         ,{field: 'spend', title: 'Spend', templet: function(d){return '$' + (d.spend || '0.00');}}
                         ,{field: 'ecpi', title: 'eCPI', templet: function(d){return '$' + (d.ecpi || '0.00');}}
-                        ,{field: 'ecpm', title: 'eCPM', templet: function(d){return '$' + (d.ecpm || '0.00');}}
+                        ,{field: 'ecpm', title: 'eCPM', templet: function(d){return '$' + (d.ecpm || '0.00');}, width:80}
                         ,{field: 'status', title: 'Status', templet: '#status', align:'center', width:70}
                         ,{fixed: 'right', width: 220, align:'center', toolbar: '#options'}
                     ]]
