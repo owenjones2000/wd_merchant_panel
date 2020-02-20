0b38f683fea6131c264f09bfd9c9816228c55bbc
Tim <twj101@163.com>
2020-02-20 13:55:55 +0800

    no message

--- resources/views/layout/common_edit.blade.php
+++ resources/views/layout/common_edit.blade.php
@@ -3,6 +3,7 @@
     $('.close-iframe').on('click',function () {
         var index = parent.layer.getFrameIndex(window.name); //先得到当前iframe层的索引
         parent.layer.close(index); //再执行关闭
+        return false;
     });
 
     @if(session('status'))
