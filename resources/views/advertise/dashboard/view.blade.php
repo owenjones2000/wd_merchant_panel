18a84f4627be4f666891adfa2cdcf7812c968b3a
Tim <twj101@163.com>
2020-02-28 11:48:20 +0800

    no message

--- resources/views/advertise/dashboard/view.blade.php
+++ resources/views/advertise/dashboard/view.blade.php
@@ -124,95 +124,95 @@
         </div>
 
 
-        <div class="layui-col-sm12">
+        {{--<div class="layui-col-sm12">--}}
 
-            <div class="layui-card">
+            {{--<div class="layui-card">--}}
 
-                <div class="layui-card-header">
+                {{--<div class="layui-card-header">--}}
 
-                    访问量
+                    {{--访问量--}}
 
-                    <div class="layui-btn-group layuiadmin-btn-group">
+                    {{--<div class="layui-btn-group layuiadmin-btn-group">--}}
 
-                        <a href="javascript:;" class="layui-btn layui-btn-primary layui-btn-xs">去年</a>
+                        {{--<a href="javascript:;" class="layui-btn layui-btn-primary layui-btn-xs">去年</a>--}}
 
-                        <a href="javascript:;" class="layui-btn layui-btn-primary layui-btn-xs">今年</a>
+                        {{--<a href="javascript:;" class="layui-btn layui-btn-primary layui-btn-xs">今年</a>--}}
 
-                    </div>
+                    {{--</div>--}}
 
-                </div>
+                {{--</div>--}}
 
-                <div class="layui-card-body">
+                {{--<div class="layui-card-body">--}}
 
-                    <div class="layui-row">
+                    {{--<div class="layui-row">--}}
 
-                        <div class="layui-col-sm8">
+                        {{--<div class="layui-col-sm8">--}}
 
-                            <div class="layui-carousel layadmin-carousel layadmin-dataview" data-anim="fade" lay-filter="LAY-index-pagetwo">
+                            {{--<div class="layui-carousel layadmin-carousel layadmin-dataview" data-anim="fade" lay-filter="LAY-index-pagetwo">--}}
 
-                                <div carousel-item id="LAY-index-pagetwo">
+                                {{--<div carousel-item id="LAY-index-pagetwo">--}}
 
-                                    <div><i class="layui-icon layui-icon-loading1 layadmin-loading"></i></div>
+                                    {{--<div><i class="layui-icon layui-icon-loading1 layadmin-loading"></i></div>--}}
 
-                                </div>
+                                {{--</div>--}}
 
-                            </div>
+                            {{--</div>--}}
 
-                        </div>
+                        {{--</div>--}}
 
-                        <div class="layui-col-sm4">
+                        {{--<div class="layui-col-sm4">--}}
 
-                            <div class="layuiadmin-card-list">
+                            {{--<div class="layuiadmin-card-list">--}}
 
-                                <p class="layuiadmin-normal-font">月访问数</p>
+                                {{--<p class="layuiadmin-normal-font">月访问数</p>--}}
 
-                                <span>同上期增长</span>
+                                {{--<span>同上期增长</span>--}}
 
-                                <div class="layui-progress layui-progress-big" lay-showPercent="yes">
+                                {{--<div class="layui-progress layui-progress-big" lay-showPercent="yes">--}}
 
-                                    <div class="layui-progress-bar" lay-percent="30%"></div>
+                                    {{--<div class="layui-progress-bar" lay-percent="30%"></div>--}}
 
-                                </div>
+                                {{--</div>--}}
 
-                            </div>
+                            {{--</div>--}}
 
-                            <div class="layuiadmin-card-list">
+                            {{--<div class="layuiadmin-card-list">--}}
 
-                                <p class="layuiadmin-normal-font">月下载数</p>
+                                {{--<p class="layuiadmin-normal-font">月下载数</p>--}}
 
-                                <span>同上期增长</span>
+                                {{--<span>同上期增长</span>--}}
 
-                                <div class="layui-progress layui-progress-big" lay-showPercent="yes">
+                                {{--<div class="layui-progress layui-progress-big" lay-showPercent="yes">--}}
 
-                                    <div class="layui-progress-bar" lay-percent="20%"></div>
+                                    {{--<div class="layui-progress-bar" lay-percent="20%"></div>--}}
 
-                                </div>
+                                {{--</div>--}}
 
-                            </div>
+                            {{--</div>--}}
 
-                            <div class="layuiadmin-card-list">
+                            {{--<div class="layuiadmin-card-list">--}}
 
-                                <p class="layuiadmin-normal-font">月收入</p>
+                                {{--<p class="layuiadmin-normal-font">月收入</p>--}}
 
-                                <span>同上期增长</span>
+                                {{--<span>同上期增长</span>--}}
 
-                                <div class="layui-progress layui-progress-big" lay-showPercent="yes">
+                                {{--<div class="layui-progress layui-progress-big" lay-showPercent="yes">--}}
 
-                                    <div class="layui-progress-bar" lay-percent="25%"></div>
+                                    {{--<div class="layui-progress-bar" lay-percent="25%"></div>--}}
 
-                                </div>
+                                {{--</div>--}}
 
-                            </div>
+                            {{--</div>--}}
 
-                        </div>
+                        {{--</div>--}}
 
-                    </div>
+                    {{--</div>--}}
 
-                </div>
+                {{--</div>--}}
 
-            </div>
+            {{--</div>--}}
 
-        </div>
+        {{--</div>--}}
     </div>
 @endsection
 
