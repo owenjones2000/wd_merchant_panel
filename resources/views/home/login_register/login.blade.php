c79518e9a3a9a028fe793b6a6ec26cb73088f681
tuwenjun <tuwenjun@wudi360.com>
2020-03-26 10:30:40 +0800

    no message

--- resources/views/home/login_register/login.blade.php
+++ resources/views/home/login_register/login.blade.php
@@ -6,11 +6,11 @@
             {{csrf_field()}}
             <div class="layui-form-item">
                 <label class="layadmin-user-login-icon layui-icon layui-icon-username" for="LAY-user-login-username"></label>
-                <input type="text" name="email" value="{{old('email')}}" lay-verify="required" placeholder="邮箱" class="layui-input">
+                <input type="text" name="email" value="{{old('email')}}" lay-verify="required" placeholder="email" autocomplete="username" class="layui-input">
             </div>
             <div class="layui-form-item">
                 <label class="layadmin-user-login-icon layui-icon layui-icon-password" for="LAY-user-login-password"></label>
-                <input type="password" name="password"  lay-verify="required" placeholder="密码" class="layui-input">
+                <input type="password" name="password"  lay-verify="required" placeholder="password" autocomplete="current-password" class="layui-input">
             </div>
 
             <div class="layui-form-item">
