6e06ea0d7f20bc38fec00eee4f056a2077e201fc
zhongixn <zhongxing@wudi360.com>
2020-06-10 15:59:26 +0800

    command

--- app/Console/Commands/CompressCommand.php
+++ app/Console/Commands/CompressCommand.php
@@ -45,10 +45,16 @@ class CompressCommand extends Command
     public function handle()
     {
         //
-        $assets = Asset::where('id', '>', 29)->where('spec->size_per_second', '>', 500000)
-            ->limit(10)->get();
-        // dd($assets->toArray(), $assets->count());
+        $assets = Asset::where('id', '>', 29)->where('spec->size_per_second', '>', 450000)
+            // ->limit(6)
+            ->get();
+        // dd( $assets->count(), app()->environment());
+        $n =0;
         foreach ($assets as $key => $asset) {
+            if ($n>=5){
+            break;
+            }
+            // dump($asset['hash'], md5_file(Storage::disk('local')->path($asset['spec']['file_path_compress'])));
             if (strpos($asset, 'mp4')) {
                 $exist = Storage::disk('local')->exists($asset['file_path']);
                 $ffprobe = FFProbe::create([
@@ -58,7 +64,6 @@ class CompressCommand extends Command
                 $oldfile = Storage::disk('local')->path($asset['file_path']);
                 if (!$exist) {
                     $save = Storage::disk('local')->put($asset['file_path'], file_get_contents($asset['url']));
-                    // exec('chmod -R 777'.storage_path());
                 }
                 // if (!isset($asset['spec']['bit_rate'])) {
                 // dd($oldfile->getPath());
@@ -80,25 +85,28 @@ class CompressCommand extends Command
                 }
                 $asset->save();
                 // dump($asset->toArray());
-                if (!isset($asset['spec']['size_per_second_compress'])) {
+                if (!isset($asset['spec']['size_per_second_compress']) && app()->environment()=='local') {
                     $file_name = date('Ymd') . time() . uniqid() . ".mp4";
                     $path = Storage::disk('local')->path('') . 'asset/';
                     $dir = 'asset/';
                     $newfile = $path . $file_name;
-                    // dump($asset->toArray());
-                    exec("ffmpeg -y -i $oldfile -b:v 1500000 $newfile");
-                    // $upload = Storage::put($dir . $file_name, file_get_contents($newfile));
-                    // // dump($video_info['bit_rate'], $upload);
-                    // $asset['hash'] = md5_file($newfile);
-                    // $asset['url'] = Storage::url($dir . $file_name);
-                    // $asset['spec'] =  array_merge($asset['spec'], [
-                    //     'size_per_second_compress' => round(filesize($newfile) / round($asset['spec']['duration'], 1)),
-                    //     'size_compress' => $this->fileSizeConvert(filesize($newfile)),
-                    //     'file_path_compress' => $dir.$file_name,
-                    // ]);
-                    // $asset->save();
+                    dump($asset->toArray());
+                    exec("ffmpeg -y -i $oldfile -b:v 1200000 $newfile");
+                    $upload = Storage::put($dir . $file_name, file_get_contents($newfile));
+                    // dump($video_info['bit_rate'], $upload);
+                    $asset['hash'] = md5_file($newfile);
+                    $asset['url'] = Storage::url($dir . $file_name);
+                    $asset['spec'] =  array_merge($asset['spec'], [
+                        'size_per_second_compress' => round(filesize($newfile) / round($asset['spec']['duration'], 1)),
+                        'size_compress' => $this->fileSizeConvert(filesize($newfile)),
+                        'file_path_compress' => $dir.$file_name,
+                    ]);
+                    $asset->save();
+                    $downloadfile = file_get_contents($asset['url']);
+                    unset($downloadfile);
                     Log::info('compress' . $asset['id']);
                     // dd($asset->toArray());
+                    $n++;
                 }
             }
         }
