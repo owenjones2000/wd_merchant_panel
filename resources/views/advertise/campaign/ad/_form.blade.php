2df7cf840d116e2077461ae64143dab35e31e633
zhongixn <zhongxing@wudi360.com>
2020-08-27 10:27:54 +0800

    opt playable

--- resources/views/advertise/campaign/ad/_form.blade.php
+++ resources/views/advertise/campaign/ad/_form.blade.php
@@ -25,7 +25,7 @@
     <div class="layui-input-block">
         <select name="playable" lay-verify="">
         <option value="0">not playable(normal end card)</option>
-        <option value="0">fake playable(html end card, but user can not play)</option>
+        <option value="0" @if($ad->assets->pluck('type_id')->contains(7)) selected @endif>fake playable(html end card, but user can not play)</option>
         <option value="1" @if($ad->assets->pluck('type_id')->contains(8)) selected @endif>real playable(html end card, and user can play)</option>
         </select> 
     </div>
